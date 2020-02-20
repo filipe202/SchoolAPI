d1694954b91953ba5979470a47ef05c44c918e99
filipe202 <filipecorreia202@gmail.com>
2020-02-20 11:56:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://dynamicforms.sgctst.msdev.pt"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/api/info', (req, res) => {
     res.status(200).send({
         name: 'Faculdade de Ciências',
